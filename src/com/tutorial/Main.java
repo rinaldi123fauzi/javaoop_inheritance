8dc9f0063fb6fd87e65e17295bb875ff57dc1110
rinalfauzi <rinalfauzi@gmail.com>
2020-05-22 16:16:41 +0700

    First push

---
+++ src/com/tutorial/Main.java
@@ -0,0 +1,13 @@
+package com.tutorial;
+
+public class Main {
+    public static void main(String[] args) {
+        Hero hero1 = new Hero();
+        hero1.name = "Otong";
+        hero1.display();
+
+        HeroStrength hero2 = new HeroStrength();
+        hero2.name = "Ucup";
+        hero2.display();
+    }
+}
